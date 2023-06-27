d3a68ca4caa5bbaa88c935d0309f8a2009a1f1e0
henriquepaulalima <henrique.lima@fluna.io>
2023-06-27 19:01:54 -0300

    Create Header component and add next settings

---
+++ src/app/stores/page.tsx
@@ -0,0 +1,7 @@
+export default function Stores() {
+  return (
+    <section>
+      Store
+    </section>
+  )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import styles from './Header.module.scss';
+import HeaderLogo from '@/assets/imgs/Mobile/Logo mobile.png';
+import menuIcon from '@/assets/imgs/menu-icon.png';
+import closeIcon from '@/assets/imgs/close-icon.png';
+import { useState } from 'react';
+import classNames from 'classnames';
+import Link from 'next/link';
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false)
+    } else if (!isMenuOpen) {
+      setIsMenuOpen(true);
+    }
+  }
+
+  return (
+    <header className={styles.header__container}>
+      <div className={styles.header_left_side__container}>
+        <img src={HeaderLogo.src} alt="Logo da Meteora" />
+      </div>
+      <div className={styles.header_right_side__container}>
+        <img
+          onClick={handleMenuClick}
+          className={styles.open_menu__icon}
+          src={menuIcon.src}
+          alt="Ícone de abrir menu"
+        />
+        <div className={classNames({
+          [styles.menu__container]: true,
+          [styles.open]: isMenuOpen,
+        })}>
+          <div className={styles.menu_title__container}>
+            <img
+              onClick={handleMenuClick}
+              className={styles.close_menu__icon}
+              src={closeIcon.src}
+              alt="Ícone de fechar menu"
+            />
+          </div>
+          <ul>
+            <li
+              className={classNames({ [styles.active]: window.location.pathname === '/home' })}
+              onClick={handleMenuClick}
+            ><Link href='/home'>Home</Link></li>
+            <li
+              className={classNames({ [styles.active]: window.location.pathname === '/stores' })}
+              onClick={handleMenuClick}
+            ><Link href='/stores'>Nossas lojas</Link></li>
+            <li
+              className={classNames({ [styles.active]: window.location.pathname === '/news' })}
+              onClick={handleMenuClick}
+            ><Link href='/news'>Novidades</Link></li>
+            <li
+              className={classNames({ [styles.active]: window.location.pathname === '/sales' })}
+              onClick={handleMenuClick}
+            ><Link href='/sales'>Promoções</Link></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
